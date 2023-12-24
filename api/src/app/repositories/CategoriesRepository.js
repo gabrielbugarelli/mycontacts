4f3a4a839c79d612db0f07b776b086fec2f5c5df
gabrielbugarelli <gabrielfonsecaoficial@gmail.com>
2023-12-24 17:11:13 -0400

    chore: organize project

---
+++ api/src/app/repositories/CategoriesRepository.js
@@ -0,0 +1,21 @@
+const db = require('../../database');
+
+class CategoriesRepository {
+  async findAll(orderBy = 'ASC') {
+    const direction = orderBy.toUpperCase() === 'DESC' ? 'DESC' : 'ASC';
+    const row = await db.query(`SELECT * FROM categories ORDER BY name ${direction};`);
+    return row;
+  }
+
+  async create({ name }) {
+    const [row] = await db.query(`
+        INSERT INTO categories(name)
+        VALUES($1)
+        RETURNING *
+    `, [name]);
+
+    return row;
+  }
+}
+
+module.exports = new CategoriesRepository();
---
+++ api/src/index.js
@@ -0,0 +1,17 @@
+require('express-async-errors');
+const express = require('express');
+const router = require('./routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+app.use((error, request, response, next) => {
+  console.log(error);
+  response.sendStatus(500);
+});
+
+app.listen(3333, () => {
+  console.log('Started on port 3333 🚀 🛸');
+});
