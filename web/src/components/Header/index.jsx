44dc407b4cc358b43e71fbc6998795a0f0e634da
gabrielbugarelli <gabrielfonsecaoficial@gmail.com>
2024-01-29 15:15:12 -0400

    feat[frontend]: Input and Select implementation

---
+++ web/src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import logo from '../../assets/images/logo.svg';
+import { Container } from './styles';
+
+export function Header() {
+  return (
+    <Container>
+      <img src={logo} alt="My Contacts" width="201" />
+    </Container>
+  );
+}
